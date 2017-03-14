21a1d211946c6b025c1cca7d23ec08d3020b41de
ROSE <ROSE>
2017-03-14 20:30:31 +0700

    tugas hari ke-dua

---
+++ Hello.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+	std::string name;
+	std::cout << "What is your name sir ";
+	std::cin >> name;
+	std::cout << "ohh, Hello " << name << " nice to meet you." << std::endl;
+	return 0;
+}
---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+	std::string name;
+	std::cout << "What is your name sir?";
+	std::cin >> name;
+	std::cout << "Hai " << name << " let us learn c++ " << std::endl;
+	return 0;
+}
