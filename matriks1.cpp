f68069c8429f60e7ddc243c185ca9f5b73957005
Rosmawati <rosmakawai@gmail.com>
2017-05-02 22:44:05 +0700

    Latihan Matriks

---
+++ matriks1.cpp
@@ -0,0 +1,16 @@
+#include <iostream> 
+using namespace std; 
+
+int main (){
+	int M [3] [3];
+	
+		for (int i = 0; i < 3; i++)
+		{
+			for (int j = 0; j < 3; j++)
+			{
+				cin >> M [i] [j];
+			}
+		}
+		cout << " Molla " << M [1] [1] << endl;
+		return 0;
+}
